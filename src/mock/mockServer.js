bc832124448bb8f4f149178f9bba225d28a68b50
lucky <11211455@qq.com>
2020-12-01 15:23:34 +0800

    动态渲染随机数据(Floor组件)

--- src/mock/mockServer.js
+++ src/mock/mockServer.js
@@ -1,7 +1,14 @@
 import Mock from "mockjs";
 import banners from "./rbanners.json";
+import floors from "./rfloors.json";
+
 
 Mock.mock("/mock/banners", "get", {
   code: 200,
   "data|5": banners,
+});
+
+Mock.mock("/mock/floors", "get", {
+  code: 200,
+  "data|3": floors,
 });
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -5,24 +5,38 @@
     <TodayRecommended></TodayRecommended>
     <Rank></Rank>
     <Like></Like>
+    <Floor v-for="floor in floors" :key="floor.id" :floor="floor"></Floor>
   </div>
 </template>
 
 <script>
+import {mapState,mapActions} from "vuex"
 import TypeNav from "@comps/TypeNav";
 import ListContainer from "./ListContainer"
 import TodayRecommended from "./TodayRecommended"
 import Rank from "./Rank"
 import Like from "./Like/Like"
-// import 
+import Floor from "./Floor/Floor"
 export default {
   name: "Home",
+  computed:{
+    ...mapState({
+      floors:(state)=>(state.home.floors)
+    })
+  },
+  methods:{
+    ...mapActions(["getFloors"])
+  },
+  mounted(){
+    this.getFloors()
+  },
   components: {
     TypeNav,
     ListContainer,
     TodayRecommended,
     Rank,
-    Like
+    Like,
+    Floor
   },
 };
 </script>
